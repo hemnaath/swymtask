d12af0aa5c6f48148fda7b3d97c19a26e9075153
Hemnaath <hemnaathrsurya@gmail.com>
2024-12-20 16:04:17 +0530

    docker file changes

--- route/taskRoute.js
+++ route/taskRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/create-task', taskController.createTask);
 router.get('/get-task', taskController.getTask);
-router.patch('/update-task/:id', taskController.updateTask);
+router.patch('/update-task/:id', taskController.updateTask); 
 router.delete('/delete-task/:id', taskController.deleteTask);
 
 module.exports = router;
